a12e8288f38e232eafeed7a5e0d5751a04b74867
martin <martin.fillon@epitech.eu>
2023-04-26 17:35:40 +0200

    refactor: :lipstick: dleted () of the returns

--- src/globbins/question_mark_handler.c
+++ src/globbins/question_mark_handler.c
@@ -26,7 +26,7 @@ static int is_matching(char *word, char *entry)
         if (word[i] != entry[i])
             return (0);
     }
-    return (1);
+    return 1;
 }
 
 static DIR *open_directory(str_t *word)
@@ -38,19 +38,19 @@ static DIR *open_directory(str_t *word)
 
     if (!str_contains(word, '/')) {
         free(word);
-        return (opendir("."));
+        return opendir(".");
     }
     while (index != -1) {
         last_slash = index;
         index = str_find(word, STR("/"), index + 1);
     }
     tmp = str_create("./");
-    tmp = *str_add(&tmp, str_tocstr(str_substr(word, 0, last_slash)));
-    dir = (opendir(str_tocstr(tmp)));
+    tmp = *str_nadd(&tmp, word->data, last_slash);
+    dir = opendir(str_tocstr(tmp));
     perror(str_tocstr(tmp));
     free(word);
     free(tmp);
-    return (dir);
+    return dir;
 }
 
 static str_t *get_pattern(str_t *word)
@@ -70,7 +70,7 @@ static str_t *get_pattern(str_t *word)
     pattern = str_create(str_tocstr(tmp));
     free(word);
     free(tmp);
-    return (pattern);
+    return pattern;
 }
 
 static vec_str_t *get_entrys(DIR *dir, str_t *pattern)
@@ -88,7 +88,7 @@ static vec_str_t *get_entrys(DIR *dir, str_t *pattern)
         entry = readdir(dir);
     }
     closedir(dir);
-    return (vec);
+    return vec;
 }
 
 vec_str_t *question_mark_handler(str_t *word)
@@ -97,18 +97,18 @@ vec_str_t *question_mark_handler(str_t *word)
     str_t *pattern = NULL;
 
     if (!str_contains(word, '?')) {
-        dprintf(2, "No match possible.\n");
-        return (NULL);
+        dprintf(2, "No match\n");
+        return NULL;
     }
     dir = open_directory(str_dup(word));
     pattern = get_pattern(str_dup(word));
     if (!dir) {
         dprintf(2, "Open failed.\n");
-        return (NULL);
+        return NULL;
     }
     if (!pattern) {
         dprintf(2, "Pattern failed.\n");
-        return (NULL);
+        return NULL;
     }
-    return (get_entrys(dir, pattern));
+    return get_entrys(dir, pattern);
 }
