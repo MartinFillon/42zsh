4e975a73926533f0648bad14aea769fecffa34d1
LeTamanoir <martin.saldinger@gmail.com>
2023-05-07 14:24:35 +0200

    fix: :bug: free key when deleting an item in a map

--- lib/my_map/remove.c
+++ lib/my_map/remove.c
@@ -5,6 +5,8 @@
 ** remove elem from map
 */
 
+#include <stdlib.h>
+
 #include "my_map.h"
 #include "my_str.h"
 #include "my_vec.h"
@@ -19,6 +21,7 @@ void map_del(map_t *map, str_t const *key)
 
     for (size_t i = 0; i < cands->size; ++i) {
         if (str_eq(cands->data[i].key, key)) {
+            free(cands->data[i].key);
             vec_remove(cands, i);
             return;
         }
