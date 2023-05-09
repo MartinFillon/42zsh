5e441dc8c7b9a43b7b2a9c972ad75fbe6560d1c9
Lou Onezime <lou.onezime@epitech.eu>
2023-05-09 10:39:43 +0200

    fix: :bug: memory leaks are mostly freed
    one error valgrind

--- src/input/exclamation.c
+++ src/input/exclamation.c
@@ -14,7 +14,7 @@
 
 static str_t *get_history_str(history_t *history, char *input)
 {
-    for (size_t i = history->entries->size - 1;i > 0; i--){
+    for (size_t i = history->entries->size - 1; i > 0; i--){
         if (strncmp(input, history->entries->data[i].command->data,
             strlen(input)) == 0){
             return history->entries->data[i].command;
@@ -45,18 +45,20 @@ static str_t *exclamation_conditions(history_t *history, str_t *input)
 int get_exclamation(str_t **line, long index, shell_t *state)
 {
     str_t *designator = NULL;
+    str_t *tmp = NULL;
 
     designator = str_ncreate((*line)->data + index, (*line)->length - index);
     str_erase_at_idx(&designator, 0);
-    designator = exclamation_conditions(&state->history, designator);
-    if (designator != NULL){
-        str_insert_str(line, index, designator);
-        str_slice(line, 0, index + designator->length + 1);
+    tmp = exclamation_conditions(&state->history, designator);
+    free(designator);
+    if (tmp != NULL){
+        str_insert_str(line, index, tmp);
+        str_slice(line, 0, index + tmp->length);
         printf("%s\n", (*line)->data);
+        free(tmp);
         return 0;
-    } else {
-        free(designator);
     }
+    free(tmp);
     dprintf(2, "%s: Event not found.\n", (*line)->data);
     return 1;
 }
